b9466624b19a220c5b23e19daeb6d57161f59f33
tryor <trywen@qq.com>
2018-09-21 19:34:02 +0800

    将int类型属性默认值入为0，使用NULL最新编译器有警告

--- event/Event.h
+++ event/Event.h
@@ -20,7 +20,7 @@
    */
   class Event {
     public:
-      Event(EventType t = NULL, void* source=NULL):type(t), source(source){
+      Event(EventType t = 0, void* source=NULL):type(t), source(source){
 
       }
       virtual ~Event(){}
